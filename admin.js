a7ae33f689378d74001d8db00fa07d65e9c7944e
Anais38 <164183901+Anais38@users.noreply.github.com>
2024-04-16 22:19:31 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,17 +1,23 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
+// admin.js
+
+// Récupération du formulaire
+const loginForm = document.getElementById('login-form');
+
+// Ajout d'un écouteur d'événement pour le soumission du formulaire
+loginForm.addEventListener('submit', function(event) {
+    // Empêcher le comportement par défaut du formulaire
     event.preventDefault();
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    // Récupérer la valeur du champ de code d'authentification
+    const codeValue = document.getElementById('code').value;
 
-    // Ici, vous enverrez les informations d'identification au backend pour vérification
-    // Exemple simplifié pour démonstration
-    if (username === 'admin' && password === 'adminpassword') {
-        // Authentification réussie, rediriger vers la page d'accueil de l'administrateur
-        window.location.href = 'admin_home.html';
+    // Vérifier si le code est valide
+    if (codeValue === '2024') {
+        // Redirection vers la page d'accueil si le code est correct
+        window.location.href = 'accueil.html';
     } else {
-        // Afficher un message d'erreur
-        document.getElementById('error-message').textContent = 'Nom d\'utilisateur ou mot de passe incorrect.';
+        // Affichage d'un message d'erreur si le code est incorrect
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.textContent = 'Code d\'authentification incorrect. Veuillez réessayer.';
     }
 });
-
